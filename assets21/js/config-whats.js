e882925a255a758bf66515a7b2706b14dfc63ac6
alvaro <alsato.650cc@gmail.com>
2021-10-07 12:19:05 -0500

    cambios assets21

--- assets21/js/config-whats.js
+++ assets21/js/config-whats.js
@@ -5,7 +5,7 @@ $(function () {
         headerTitle: 'Chatea con nostros en Whats App!', //Popup Title
         popupMessage: 'Hola, como puedo ayudarte?', //Popup Message
         showPopup: true, //Enables popup display
-        buttonImage: '<img src="img/whatsapp.svg" />', //Button Image
+        buttonImage: '<img src="assets21/img/whatsapp.svg" />', //Button Image
         //headerColor: 'crimson', //Custom header color
         //backgroundColor: 'crimson', //Custom background button color
         position: "right" //Position: left | right
