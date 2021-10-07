ecd5732422ec1f0a919e09b6f94660e9c8b10815
alvaro <alsato.650cc@gmail.com>
2021-10-07 11:03:13 -0500

    se añadio plugin de whats

---
+++ js/config-whats.js
@@ -0,0 +1,13 @@
+
+$(function () {
+    $('#WAButton').floatingWhatsApp({
+        phone: '+525563269449', //WhatsApp Business phone number
+        headerTitle: 'Chatea con nostros en Whats App!', //Popup Title
+        popupMessage: 'Hola, como puedo ayudarte?', //Popup Message
+        showPopup: true, //Enables popup display
+        buttonImage: '<img src="img/whatsapp.svg" />', //Button Image
+        //headerColor: 'crimson', //Custom header color
+        //backgroundColor: 'crimson', //Custom background button color
+        position: "right" //Position: left | right
+    });
+});
