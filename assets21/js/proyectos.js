cf456c1708c8c0de122335bbe09ac38b0bf29707
alvaro <alsato.650cc@gmail.com>
2021-10-12 17:32:54 -0500

    changes

--- assets21/js/proyectos.js
+++ assets21/js/proyectos.js
@@ -1,18 +1,22 @@
 function ejceutar() {
+  let categoria = document.querySelector("#categoria").value;
+  let selected = document.querySelector("#select").value;
 
-    let categoria = document.querySelector('#categoria').value;
-    let selected = document.querySelector('#select').value;
+  if (categoria == "") {
+    categoria = "7110578";
+  }
 
+  const url =
+    "https://api.themoviedb.org/4/list/" +
+    categoria +
+    "?page=1&api_key=15c78749fb202cfe47bdd8f4989396e2&language=es&sort_by=" +
+    selected +
+    "";
 
-    const url = "https://api.themoviedb.org/4/list/"+categoria+"?page=1&api_key=15c78749fb202cfe47bdd8f4989396e2&language=es&sort_by="+selected+"";
-    
-
-  fetch(
-    url  
-  )
+  fetch(url)
     .then((rest) => rest.json())
     .then((json) => {
-      const resultados = json.results
+      const resultados = json.results;
       const agregar = document.querySelector("#proyectos");
       agregar.innerHTML = resultados
         .map(
@@ -20,14 +24,22 @@ function ejceutar() {
             `
                 <div class="col-sm-12 col-md-6 mb-3">
                     <div class="cardP p-4">
-                        <img class="proyectosimg" src="https://image.tmdb.org/t/p/w500${resultado.backdrop_path ? resultado.backdrop_path : resultado.poster_path}" alt="poster de pelicula" width="100%">
+                        
+                        
+                        <img class="proyectosimg" src="https://image.tmdb.org/t/p/w500${resultado.backdrop_path?resultado.backdrop_path:resultado.poster_path}" 
+                         alt="poster de pelicula" 
+                         width="100%"
+                         onclick="${resultado.id}"
+                        />
                         <div class="titulo">
                             ${resultado.name ? resultado.name : resultado.title}
                         </div>
                         <div class="calificacion mb-3">
                             <div class="input-group">
                                 <div class="minicard">    
-                                    <img src="/assets21/img/swiper-slider/imdb.png" alt="" /> <strong>${resultado.vote_average}</strong>/10
+                                    <img src="/assets21/img/swiper-slider/imdb.png" alt="" /> <strong>${
+                                      resultado.vote_average
+                                    }</strong>/10
                                 </div>
                                 <div class="minicard mx-4">
                                     <strong style="color: #6A6A6A;">HD</strong>
@@ -37,13 +49,12 @@ function ejceutar() {
                                 </div>
                             </div>
                         </div>
-                        <div class="director">
-
-                            Director: <strong style="color: #101010;">XXXXX</strong>
-
-                        </div>
                         <div class="lanzamiento">
-                            Lanzamiento: <strong style="color: #101010;">${resultado.release_date ? resultado.release_date : ''}</strong>
+                            Lanzamiento: <strong style="color: #101010;">${
+                              resultado.release_date
+                                ? resultado.release_date
+                                : ""
+                            }</strong>
                         </div>
                     </div>
                 </div>
