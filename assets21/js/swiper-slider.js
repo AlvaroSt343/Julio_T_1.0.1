a11cab082173d8dcdeaa0977042d86b93bf24e59
alvaro <alsato.650cc@gmail.com>
2021-10-25 15:57:56 -0500

    dfg

--- assets21/js/swiper-slider.js
+++ assets21/js/swiper-slider.js
@@ -10,7 +10,7 @@ var swiper = new Swiper(".mySwiper", {
         stretch: 0,
         depth: 100,
         modifier: 1,
-        slideShadows: true,
+        slideShadows: false,
         
     },
     autoplay:{
